d192272ba7b663c9542ca3abc71e28fcb476204f
Cnix24 <calvinattl24@gmail.com>
2019-07-17 14:48:39 -0600

    Update

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step113Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Class1 instantiate = new Class1();
+            //call the method in the class, passing in two numbers
+            instantiate.MethodA(7, 11);
+
+            //call the method in the class, specifying the parameters by name
+            instantiate.MethodA(x: 7, y: 11);
+
+        }
+    }
+}
